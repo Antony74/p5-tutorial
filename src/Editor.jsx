234e2e37275f75f1b6e10ed01e21ac2bccbdef18
Antony74 <akb@akb.me.uk>
2021-08-22 18:37:46 +0100

    Can now execute code

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -2,19 +2,31 @@ import * as React from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'react-codemirror2';
 import 'codemirror/mode/javascript/javascript';
+import 'p5';
 
 const Editor = () => {
-  const [value, setValue] = React.useState("console.log('Hello world');");
+  const [state, setState] = React.useState({editor: "console.log('Hello world');"});
+  const ref = React.useRef();
+
+  const onRun = () => {
+    const script = document.createElement('script');
+    script.text = state.editor;
+    ref.current.append(script);
+  };
 
   return (
-    <CodeMirror
-      options={{ mode: 'javascript', theme: 'material', lineNumbers: true }}
-      value={value}
-      onBeforeChange={(_editor, _data, value) => {
-        setValue(value);
-      }}
-    ></CodeMirror>
+    <div ref={ref}>
+      <CodeMirror
+        options={{ mode: 'javascript', theme: 'material', lineNumbers: true }}
+        value={state.editor}
+        onBeforeChange={(_editor, _data, value) => {
+          setState({...state, editor: value});
+        }}
+      ></CodeMirror>
+      <button onClick={onRun}>Run</button>
+    </div>
   );
+
 };
 
 export default Editor;
