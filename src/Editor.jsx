2cbaaad259c0283820b851bbe5263be1106373f7
Antony74 <akb@akb.me.uk>
2021-08-22 16:15:42 +0100

    Prettier

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-import { Controlled as CodeMirror } from "react-codemirror2";
-import "react-codemirror2";
-import "codemirror/mode/javascript/javascript";
+import * as React from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import 'react-codemirror2';
+import 'codemirror/mode/javascript/javascript';
 
 const Editor = () => {
   const [value, setValue] = React.useState("console.log('Hello world');");
 
   return (
     <CodeMirror
-      options={{ mode: "javascript", theme: "material", lineNumbers: true }}
+      options={{ mode: 'javascript', theme: 'material', lineNumbers: true }}
       value={value}
       onBeforeChange={(_editor, _data, value) => {
         setValue(value);
