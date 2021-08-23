c6840ff9b5646a0a22fb00ff840d383849064b17
Antony74 <akb@akb.me.uk>
2021-08-23 12:05:35 +0100

    Refactor to allow for seperate editor and log

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,32 +1,61 @@
 import * as React from 'react';
-import { Controlled as CodeMirror } from 'react-codemirror2';
+import { Controlled } from 'react-codemirror2';
 import 'react-codemirror2';
 import 'codemirror/mode/javascript/javascript';
 import 'p5';
 
+const useGetSet = (initialValue) => {
+  const [state, setState] = React.useState(initialValue);
+  return { get: () => state, set: (value) => setState(value) };
+};
+
+const CodeMirror = (props) => (
+  <Controlled
+    {...props}
+    value={props.hook.get()}
+    onBeforeChange={(_editor, _data, value) => {
+      props.hook.set(value);
+    }}
+  ></Controlled>
+);
+
 const Editor = () => {
-  const [state, setState] = React.useState({editor: "console.log('Hello world');"});
-  const ref = React.useRef();
+  const editorHook = useGetSet("console.log('Hello world');");
+  const logHook = useGetSet('');
+
+  const Code = () => (
+    <CodeMirror
+      options={{ mode: 'javascript', theme: 'material', lineNumbers: true }}
+      hook={editorHook}
+    ></CodeMirror>
+  );
+
+  const Log = () => {
+    if (logHook.get().length) {
+      return (
+        <CodeMirror
+          options={{ theme: 'material' }}
+          hook={logHook}
+        ></CodeMirror>
+      );
+    } else {
+      return <></>;
+    }
+  };
 
   const onRun = () => {
-    eval(`runUserCode = () => {${state.editor}}`);
+    eval(`runUserCode = () => {${editorHook.get()}}`);
     // eslint-disable-next-line no-undef
     runUserCode();
   };
 
   return (
-    <div ref={ref}>
-      <CodeMirror
-        options={{ mode: 'javascript', theme: 'material', lineNumbers: true }}
-        value={state.editor}
-        onBeforeChange={(_editor, _data, value) => {
-          setState({...state, editor: value});
-        }}
-      ></CodeMirror>
+    <div>
+      <Code />
+      <Log />
       <button onClick={onRun}>Run</button>
     </div>
   );
-
 };
 
 export default Editor;
